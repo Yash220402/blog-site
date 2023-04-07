a33a20fcb91639a737398198327d829949399ea2
Yash220402 <yash220402@gmail.com>
2023-04-07 17:54:19 +0530

    add share via email

--- myblog/blog/views.py
+++ myblog/blog/views.py
@@ -3,9 +3,9 @@ from .models import Post
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 # Create your views here.
-def post_list(request):
-    posts = Post.published.all()
-    return render(request,'blog/post/list.html',{'posts': posts})
+# def post_list(request):
+#     posts = Post.published.all()
+#     return render(request,'blog/post/list.html',{'posts': posts})
 
 def post_detail(request, year, month, day, post):
     post = get_object_or_404(Post, slug=post,
@@ -16,23 +16,23 @@ def post_detail(request, year, month, day, post):
     
     return render(request, 'blog/post/detail.html', {'post': post})
 
-def post_list(request):
-    object_list = Post.published.all()
-    paginator = Paginator(object_list, 3) # 3 posts in each page
-    page = request.GET.get('page')
-    try:
-        posts = paginator.page(page)
-    except PageNotAnInteger:
-        # if page is not an integer deliver the first page
-        posts = paginator.page(1)
-    except EmptyPage:
-        # if page is out of range then deliver the last page
-        posts = paginator.page(paginator.num_pages)
+# def post_list(request):
+#     object_list = Post.published.all()
+#     paginator = Paginator(object_list, 3) # 3 posts in each page
+#     page = request.GET.get('page')
+#     try:
+#         posts = paginator.page(page)
+#     except PageNotAnInteger:
+#         # if page is not an integer deliver the first page
+#         posts = paginator.page(1)
+#     except EmptyPage:
+#         # if page is out of range then deliver the last page
+#         posts = paginator.page(paginator.num_pages)
     
-    return render(request,
-                  'blog/post/list.html',
-                  {'page': page,
-                  'posts': posts})
+#     return render(request,
+#                   'blog/post/list.html',
+#                   {'page': page,
+#                   'posts': posts})
 
 from django.views.generic import ListView
 
@@ -41,3 +41,21 @@ class PostListView(ListView):
     context_object_name = 'posts'
     paginate_by = 3
     template_name = 'blog/post/list.html'
+
+
+from .forms import EmailPostForm
+
+def post_share(request, post_id):
+    # Retrieve post by id
+    post = get_object_or_404(Post, id=post_id, status='published')
+    
+    if request.method == 'POST':
+        # form was submitted
+        form = EmailPostForm(request.POST)
+        if form.is_valid():
+            # form fields passed validation
+            cd = form.cleaned_data
+            # ... send data
+    else:
+        form = EmailPostForm()
+    return render(request, 'blog/post/share.html', {'post': post, 'form': form})
