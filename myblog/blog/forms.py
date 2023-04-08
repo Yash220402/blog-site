0f9102ccff82871a6a3a66d6eb6d54a2df235e3a
Yash220402 <yash220402@gmail.com>
2023-04-09 00:54:31 +0530

    create comment model

--- myblog/blog/forms.py
+++ myblog/blog/forms.py
@@ -4,5 +4,11 @@ class EmailPostForm(forms.Form):
     name = forms.CharField(max_length=25)
     email = forms.EmailField()
     to = forms.EmailField()
-    comments = forms.CharField(required=False,
-                               widget=forms.Textarea)
+    comments = forms.CharField(required=False, widget=forms.Textarea)
+
+from .models import Comment
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ('name', 'email', 'body')
