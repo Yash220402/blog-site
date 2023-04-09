0d70909f15d7754e235408d3706656ecb8c0a740
Yash220402 <yash220402@gmail.com>
2023-04-09 15:55:52 +0530

    add post similarity suggestion

--- myblog/blog/views.py
+++ myblog/blog/views.py
@@ -4,6 +4,9 @@ from django.views.generic import ListView
 
 from .models import Post, Comment
 from .forms import EmailPostForm, CommentForm
+from taggit.models import Tag
+
+from django.db.models import Count
 
 # Create your views here.
 # def post_list(request):
@@ -34,20 +37,25 @@ def post_detail(request, year, month, day, post):
     else:
         comment_form = CommentForm()
     
+    # List similar posts
+    post_tags_ids = post.tags.values_list('id', flat=True)
+    similar_posts = Post.published.filter(tags__in=post_tags_ids).exclude(id=post.id)
+    similar_posts = similar_posts.annotate(same_tags=Count('tags')).order_by('-same_tags', '-publish')[:4]
+    
     return render(request, 'blog/post/detail.html', 
                             {'post': post,
                              'comments': comments,
                              'new_comment': new_comment,
-                             'comment_form': comment_form})
+                             'comment_form': comment_form,
+                             'similar_posts': similar_posts})
 
-from taggit.models import Tag
 def post_list(request, tag_slug=None):
     object_list = Post.published.all()
     tag = None 
     
     if tag_slug:
         tag = get_object_or_404(Tag, slug=tag_slug)
-        object_list = object_list.filter(tags__in=tag)
+        object_list = object_list.filter(tags__in=[tag])
 
 
     paginator = Paginator(object_list, 3) # 3 posts in each page
