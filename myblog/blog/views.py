05f01484da7554421e62d3ac9d66a19b199921da
Yash220402 <yash220402@gmail.com>
2023-04-10 08:24:51 +0530

    create feeds

--- myblog/blog/views.py
+++ myblog/blog/views.py
@@ -72,8 +72,8 @@ def post_list(request, tag_slug=None):
     return render(request,
                   'blog/post/list.html',
                   {'page': page,
-                  'posts': posts,
-                  'tag': tag})
+                   'posts': posts,
+                   'tag': tag})
 
 # class PostListView(ListView):
 #     queryset = Post.published.all()
