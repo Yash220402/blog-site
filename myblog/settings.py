ff2674b727c4a68c3974c8fe6866a1ee5704a919
Yash220402 <yash220402@gmail.com>
2023-04-13 13:44:47 +0530

    Added files and changes required for deployment

--- myblog/settings.py
+++ myblog/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 import os
-SECRET_KEY = os.eviron.get('DJANGO_SECRET_KEY', 'django-insecure-nx)cwygo89y*zof@&6bj@%o(l^trr7v%&zzvkor0q&dcwnk*0n')
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-nx)cwygo89y*zof@&6bj@%o(l^trr7v%&zzvkor0q&dcwnk*0n')
 # SECRET_KEY = 'django-insecure-nx)cwygo89y*zof@&6bj@%o(l^trr7v%&zzvkor0q&dcwnk*0n'
 
 # SECURITY WARNING: don't run with debug turned on in production!
@@ -49,6 +49,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -133,3 +134,22 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Update database configuration from $DATABASE_URL.
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
+
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/4.0/howto/static-files/
+
+# The absolute path to the directory where collectstatic will collect static files for deployment.
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+# The URL to use when referring to static files (where they will be served from)
+STATIC_URL = '/static/'
+
+# Simplified static file serving.
+# https://pypi.org/project/whitenoise/
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
